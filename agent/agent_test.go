71533854354106a145f29a787161cb495b5b9f35
Arthur <arthur@corenzan.com>
2019-04-07 16:29:55 -0300

    Add test for basic API request

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -1,13 +1,58 @@
 package agent
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+// RoundTripFunc .
+type RoundTripFunc func(req *http.Request) *http.Response
+
+// RoundTrip .
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+//NewTestClient returns *http.Client with Transport replaced to avoid making real calls
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: RoundTripFunc(fn),
+	}
+}
 
 func TestNew(t *testing.T) {
-	a := New("endpoint", "key")
-	if a.endpoint != "endpoint" {
+	a := New("https://api", "123")
+	if a.endpoint != "https://api" {
 		t.Fail()
 	}
-	if a.key != "key" {
+	if a.key != "123" {
 		t.Fail()
 	}
+}
+func TestAPIRequest(t *testing.T) {
+	a := New("https://api", "123")
+
+	method := "GET"
+	path := "/"
+
+	a.client = NewTestClient(func(req *http.Request) *http.Response {
+		if req.Method != method {
+			t.Fail()
+		}
+		if req.URL.String() != a.endpoint+path {
+			t.Fail()
+		}
+		if req.Header.Get("Authorization") != "Bearer "+a.key {
+			t.Fail()
+		}
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(`OK`)),
+			Header:     make(http.Header),
+		}
+	})
+
+	a.apiRequest(method, path, nil)
 }
