9e7be1c2c8e1171e76738639de367907f88473c7
Arthur <arthur@corenzan.com>
2019-04-11 19:11:57 -0300

    Update schema and subsequent changes

--- api/schema.sql
+++ api/schema.sql
@@ -1,14 +1,26 @@
-create table websites (
+create table if not exists websites (
     id serial primary key,
-    timestamp timestamp not null default now(),
+    updated timestamp not null default now(),
     url text not null,
     status int default 0
 );
 
-create table checks (
+create table if not exists checks (
     id serial primary key,
-    timestamp timestamp not null default now(),
+    created timestamp not null default now(),
     website_id integer references websites (id),
     status integer not null,
     latency integer not null
 );
+
+-- 2019-04-10
+create or replace function public.percentage(a numeric, b numeric) 
+returns numeric language sql as $function$
+    select case when b = 0 then 0 else round(100.0 / b * a) end;
+$function$;
+
+-- 2019-04-11 11:23:31
+alter table websites rename column timestamp to updated;
+alter table websites alter updated type timestamptz using updated at time zone 'utc';
+alter table checks rename column timestamp to created;
+alter table checks alter created type timestamptz using created at time zone 'utc';
