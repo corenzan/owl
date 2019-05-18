693ee95e32a9a1f886890f3f3e22208e6636f7c8
Arthur <arthur@corenzan.com>
2019-05-18 16:03:41 -0300

    Accept URL as flag

--- agent/cli/main.go
+++ agent/cli/main.go
@@ -4,19 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/corenzan/owl/agent"
 	"github.com/corenzan/owl/agent/api"
 )
 
 var (
-	endpoint, key string
+	endpoint, key, url string
 )
 
 func init() {
 	flag.StringVar(&endpoint, "endpoint", os.Getenv("API_URL"), "endpoint for the api, also read from API_URL")
 	flag.StringVar(&key, "key", os.Getenv("API_KEY"), "key for api authorization, also read from API_KEY")
+	flag.StringVar(&url, "url", "", "skip the API and just check given URL")
 }
 
 func main() {
@@ -24,23 +24,23 @@ func main() {
 
 	a := agent.New(endpoint, key)
 
-	url := flag.Arg(0)
-	if url == "" {
-		a.Run()
-	} else {
+	if url != "" {
 		check, err := a.Check(&api.Website{
 			URL: url,
 		})
 		if err == nil {
-			fmt.Printf("%10s: %s\n", "URL", url)
-			fmt.Printf("%10s: %d\n", "StatusCode", check.StatusCode)
-			fmt.Printf("%10s: %4dms\n", "DNS", check.Breakdown.DNS/time.Millisecond)
-			fmt.Printf("%10s: %4dms\n", "TLS", check.Breakdown.TLS/time.Millisecond)
-			fmt.Printf("%10s: %4dms\n", "Connection", check.Breakdown.Connection/time.Millisecond)
-			fmt.Printf("%10s: %4dms\n", "Wait", check.Breakdown.Wait/time.Millisecond)
-			fmt.Printf("%10s: %4dms\n", "Total", check.Duration/time.Millisecond)
+			fmt.Printf("%12s: %s\n", "URL", url)
+			fmt.Printf("%12s: %d\n", "StatusCode", check.StatusCode)
+			fmt.Printf("%12s: %dms\n", "DNS", check.Breakdown.DNS)
+			fmt.Printf("%12s: %dms\n", "TLS", check.Breakdown.TLS)
+			fmt.Printf("%12s: %dms\n", "Connection", check.Breakdown.Connection)
+			fmt.Printf("%12s: %dms\n", "Application", check.Breakdown.Application)
+			fmt.Printf("%12s: %dms\n", "Total", check.Duration)
 		} else {
-			fmt.Println("agent/cli: check failed", err)
+			fmt.Println("agent/cli:", err)
 		}
+		return
 	}
+
+	a.Run()
 }
