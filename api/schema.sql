090bd340a381148b1f3b9e31c28a83ee4ab2bd4d
Arthur <arthur@corenzan.com>
2019-05-19 17:13:33 -0300

    Add columns status and updated_at to websites

--- api/schema.sql
+++ api/schema.sql
@@ -1,22 +1,26 @@
 
-create function percentage(v numeric, t numeric)
+create or replace function percentage(v numeric, t numeric)
 returns numeric as $$
     begin return case when t = 0 then 0 else v / t * 100.0 end; end;
 $$ language plpgsql;
 
-create function timestamptz_in_of(t timestamptz, i text, r timestamptz)
+create or replace function timestamptz_in_of(t timestamptz, i text, r timestamptz)
 returns boolean as $$
     begin return t::timestamptz between r::timestamptz and r::timestamptz + i::interval; end;
 $$ language plpgsql;
 
+create type website_status as enum ('unknown', 'up', 'maintenance', 'down');
+
 create table websites (
     id serial primary key,
+    updated_at timestamptz not null default current_timestamp,
+    status website_status not null default 'unknown',
     url text not null
 );
 
 create table checks (
     id serial primary key,
-    checked_at timestamp not null default now(),
+    checked_at timestamptz not null default current_timestamp,
     website_id integer references websites (id),
     status_code numeric not null,
     duration numeric not null,
