fcb5ac9e6b17f2f307836e91854a61d2e4765cb7
Arthur <arthur@corenzan.com>
2019-05-22 19:53:41 -0300

    Update website stats and include Apdex

--- client/src/components/Website/index.js
+++ client/src/components/Website/index.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect, useContext } from "react";
 import Moment from "react-moment";
 import c from "classnames";
 
-import Indicator from "../Indicator";
-import { appContext } from "../App";
 import api from "../../api.js";
+import { appContext } from "../App";
+import Indicator from "../Indicator";
 
 import style from "./style.module.css";
 
 export default ({ website, extended, onClick }) => {
-    const [stats, setStats] = useState({});
+    const [stats, setStats] = useState(null);
     const { period } = useContext(appContext);
 
     useEffect(() => {
@@ -18,41 +18,73 @@ export default ({ website, extended, onClick }) => {
 
     return (
         <div className={style.website} onClick={onClick}>
-            <div className={style.segment}>
-                <Indicator status={website.status} />
-            </div>
-            <div className={c(style.segment, style.name)}>
-                {website.url}
-                <small className={style.label}>
-                    <Moment date={website.updatedAt} fromNow />
-                </small>
+            <div className={style.row}>
+                <div className={style.segment}>
+                    <Indicator status={website.status} />
+                </div>
+                <div className={c(style.segment, style.name)}>
+                    {website.url}
+                    <small className={style.label}>
+                        <Moment date={website.updatedAt} fromNow />
+                    </small>
+                </div>
+                {extended ? (
+                    <>
+                        <div className={c(style.segment, style.desktop)}>
+                            <small className={style.label}>Apdex</small>
+                            {stats ? stats.apdex.toFixed(2) : "0.00"}
+                        </div>
+                        <div className={c(style.segment, style.desktop)}>
+                            <small className={style.label}>Average</small>
+                            {stats ? (stats.average / 1000).toFixed(2) : "0.00"}s
+                        </div>
+                        <div className={c(style.segment, style.desktop)}>
+                            <small className={style.label}>Lowest</small>
+                            {stats ? (stats.lowest / 1000).toFixed(2) : "0.00"}s
+                        </div>
+                        <div className={c(style.segment, style.desktop)}>
+                            <small className={style.label}>Highest</small>
+                            {stats ? (stats.highest / 1000).toFixed(2) : "0.00"}s
+                        </div>
+                        <div className={c(style.segment, style.desktop)}>
+                            <small className={style.label}>Checks</small>
+                            {stats ? stats.count : "0"}
+                        </div>
+                        <div className={style.segment}>
+                            <small className={style.label}>Uptime</small>
+                            {stats ? (stats.uptime % 1 > 0 ? stats.uptime.toFixed(4) : stats.uptime) : 0}%
+                        </div>
+                    </>
+                ) : (
+                    <div className={style.segment}>
+                        {stats ? (stats.uptime % 1 > 0 ? stats.uptime.toFixed(4) : stats.uptime) : 0}%
+                    </div>
+                )}
             </div>
             {extended ? (
-                <>
-                    <div className={c(style.segment, style.spaced, style.extended)}>
-                        <small className={style.label}>Checks</small>
-                        {stats ? stats.count : "0"}
+                <div className={c(style.row, style.justified, style.mobile)}>
+                    <div className={style.segment}>
+                        <small className={style.label}>Apdex</small>
+                        {stats ? stats.apdex.toFixed(2) : "0.00"}
                     </div>
-                    <div className={c(style.segment, style.spaced)}>
+                    <div className={style.segment}>
                         <small className={style.label}>Average</small>
                         {stats ? (stats.average / 1000).toFixed(2) : "0.00"}s
                     </div>
-                    <div className={c(style.segment, style.spaced, style.extended)}>
+                    <div className={style.segment}>
                         <small className={style.label}>Lowest</small>
                         {stats ? (stats.lowest / 1000).toFixed(2) : "0.00"}s
                     </div>
-                    <div className={c(style.segment, style.spaced, style.extended)}>
+                    <div className={style.segment}>
                         <small className={style.label}>Highest</small>
                         {stats ? (stats.highest / 1000).toFixed(2) : "0.00"}s
                     </div>
-                    <div className={c(style.segment, style.spaced)}>
-                        <small className={style.label}>Uptime</small>
-                        {stats.uptime % 1 > 0 ? stats.uptime.toFixed(4) : stats.uptime}%
+                    <div className={style.segment}>
+                        <small className={style.label}>Checks</small>
+                        {stats ? stats.count : "0"}
                     </div>
-                </>
-            ) : (
-                <div className={style.segment}>{stats.uptime % 1 > 0 ? stats.uptime.toFixed(4) : stats.uptime}%</div>
-            )}
+                </div>
+            ) : null}
         </div>
     );
 };
