29b635356346aaca9bb34199dc496d167a0e35ea
Arthur <arthur@corenzan.com>
2019-05-22 00:50:22 -0300

    Add quick search to the menu

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -10,20 +10,38 @@ import style from "./style.module.css";
 export default () => {
     const [match, params] = useRoute("/websites/:id");
     const [websites, update] = useState([]);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         api.websites().then(update);
     }, []);
 
+    const onKeyDown = e => {
+        if (e.code === "Escape") {
+            setQuery("");
+        }
+    };
+
     return (
         <div className={style.menu}>
-            {websites.map(website => (
-                <Link key={website.id} href={`/websites/${website.id}`}>
-                    <a href="/" className={c(style.row, { [style.selected]: match && params.id === String(website.id) })}>
-                        <Website website={website} />
-                    </a>
-                </Link>
-            ))}
+            <header className={style.search}>
+                <input
+                    type="search"
+                    value={query}
+                    placeholder="Search…"
+                    onChange={e => setQuery(e.target.value)}
+                    onKeyDown={e => onKeyDown(e)}
+                />
+            </header>
+            {websites
+                .filter(website => website.url.indexOf(query.toLowerCase()) > -1)
+                .map(website => (
+                    <Link key={website.id} href={`/websites/${website.id}`}>
+                        <a href="/" className={c(style.row, { [style.selected]: match && params.id === String(website.id) })}>
+                            <Website website={website} />
+                        </a>
+                    </Link>
+                ))}
         </div>
     );
 };
