c4e4c1176306b491360ea09c08efd2931b728a24
Arthur <arthur@corenzan.com>
2019-05-23 14:53:55 -0300

    Fix responsive behavior and some scroll issues, still WIP

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import c from "classnames";
 
 import style from "./style.module.css";
@@ -6,11 +6,11 @@ import moment from "moment";
 
 const maxLatency = 10000;
 const height = 128;
-const barWidth = 12;
+const step = 12;
 const barStroke = 2;
 
 const Bar = ({ index, check }) => {
-    const x = barWidth * index;
+    const x = step * index;
     const y = check.latency.total / maxLatency;
 
     const dns = (check.latency.dns / (check.latency.total || 1)) * 100;
@@ -40,71 +40,102 @@ const Bar = ({ index, check }) => {
                 </defs>
             ) : null}
             <rect
-                width={barWidth - barStroke * 2}
+                width={step - barStroke * 2}
                 height={Math.round((height - 1) * y) + 1}
                 x={x + barStroke}
                 y={Math.round(height * (1 - y))}
                 fill={check.latency.total > 0 ? "url(#fill" + index + ")" : "#aaa"}
                 className={style.bar}
             />
-            <rect width={barWidth} height={height} x={x} className={c(style.overlay, { [style.bad]: check.result !== "up" })} />
+            <rect width={step} height={height} x={x} className={c(style.overlay, { [style.bad]: check.result !== "up" })} />
         </g>
     );
 };
 
-const calculateAvailableWidth = element => {
-    const style = getComputedStyle(element.parentElement);
-    return element.parentElement.clientWidth - parseInt(style.paddingLeft, 10) - parseInt(style.paddingRight, 10);
-};
-
-let previousClientX = 0;
-
 export default ({ checks }) => {
-    const root = useRef(null);
-    const [limit, setLimit] = useState(0);
-    const [offset, setOffset] = useState(0);
+    const ref = useRef(null);
 
-    const width = barWidth * limit;
+    const [availableSpace, setAvailableSpace] = useState(0);
+    useEffect(() => {
+        const calculateAvailableSpace = e => {
+            const parent = ref.current.parentElement;
+            const style = getComputedStyle(parent);
+            setAvailableSpace(parent.clientWidth - parseInt(style.paddingLeft, 10) - parseInt(style.paddingRight, 10));
+        };
+        calculateAvailableSpace();
+        window.addEventListener("resize", calculateAvailableSpace);
+        return () => {
+            window.removeEventListener("resize", calculateAvailableSpace);
+        };
+    }, []);
+
+    const limit = Math.floor(availableSpace / step);
+    const width = step * limit;
     const viewBox = `0 0 ${width} ${height}`;
+    const maxOffset = checks.length - limit;
+
+    const [offset, setOffset] = useState(Infinity);
 
     useEffect(() => {
-        const availableWidth = calculateAvailableWidth(root.current);
-        const limit = Math.floor(availableWidth / barWidth);
-        setLimit(limit);
-        setOffset(Math.max(checks.length - limit, 0));
-    }, [checks]);
+        setOffset(offset > maxOffset ? maxOffset : offset);
+    }, [maxOffset]);
 
-    const onScroll = e => {
-        if (checks.length < limit) {
-            return;
-        }
-        let delta = 0;
-        if (e.type === "wheel") {
-            delta = e.deltaY || e.deltaX;
-        } else {
-            delta = previousClientX - e.touches[0].clientX;
-            previousClientX = e.touches[0].clientX;
-        }
-        if (delta > 0) {
-            setOffset(Math.min(offset + 1, checks.length - limit));
-        } else {
-            setOffset(Math.max(offset - 1, 0));
-        }
-    };
+    const [previousClientX, setPreviousClientX] = useState(0);
+    const [previousOffset, setPreviousOffset] = useState(0);
 
-    const onTouchStart = e => {
-        previousClientX = e.touches[0].clientX;
-    };
+    const onWheel = useCallback(
+        e => {
+            if (checks.length < limit) {
+                return;
+            }
+            const delta = e.deltaY || e.deltaX;
+            if (delta > 0) {
+                setOffset(Math.min(offset + Math.round(delta / step), maxOffset));
+            } else {
+                setOffset(Math.max(offset + Math.round(delta / step), 0));
+            }
+            e.stopPropagation();
+        },
+        [offset, maxOffset]
+    );
+
+    const onTouchMove = useCallback(
+        e => {
+            if (checks.length < limit) {
+                return;
+            }
+            const delta = previousClientX - e.touches[0].clientX;
+            if (delta > 0) {
+                setOffset(Math.min(previousOffset + Math.round(delta / step), maxOffset));
+            } else {
+                setOffset(Math.max(previousOffset + Math.round(delta / step), 0));
+            }
+            e.stopPropagation();
+        },
+        [maxOffset, previousClientX, previousOffset]
+    );
+
+    const onTouchStart = useCallback(
+        e => {
+            setPreviousClientX(e.touches[0].clientX);
+            setPreviousOffset(offset);
+            // e.preventDefault();
+        },
+        [offset]
+    );
+
+    // console.log(checks.length, offset, limit);
 
     return (
         <svg
+            className={style.chart}
             role="img"
-            ref={root}
+            ref={ref}
             viewBox={viewBox}
             height={height}
             width={width}
-            onTouchMove={e => onScroll(e)}
-            onWheel={e => onScroll(e)}
+            onTouchMove={e => onTouchMove(e)}
+            onWheel={e => onWheel(e)}
             onTouchStart={e => onTouchStart(e)}
         >
             {checks.slice(offset, offset + limit).map((check, index) => (
